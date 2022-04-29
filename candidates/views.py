35e4b4e01ec7fb3346f27bbe0460e5f3c051edce
Daniel Kim <DanielKim1@protonmail.com>
2022-04-29 10:32:51 -0400

    Sort NA in back

--- candidates/views.py
+++ candidates/views.py
@@ -16,10 +16,12 @@ def index(request):
             result.append(row)
 
     sort = request.GET.get("sort")
+    # https://stackoverflow.com/questions/18411560/sort-list-while-pushing-none-values-to-the-end
     if sort:
         key, direction = sort.split(":")
         result.sort(
-            key=lambda candidate: candidate[key], reverse=direction.startswith("d")
+            key=lambda candidate: (candidate[key] != "NA", candidate[key]),
+            reverse=direction.startswith("d"),
         )
 
     return HttpResponse(dumps(result))
@@ -36,10 +38,12 @@ def election(request, state, year):
                 result.append(row)
 
     sort = request.GET.get("sort")
+    # https://stackoverflow.com/questions/18411560/sort-list-while-pushing-none-values-to-the-end
     if sort:
         key, direction = sort.split(":")
         result.sort(
-            key=lambda candidate: candidate[key], reverse=direction.startswith("d")
+            key=lambda candidate: (candidate[key] != "NA", candidate[key]),
+            reverse=direction.startswith("d"),
         )
 
     if result:
