5ba6a4291db7301a286218ab6e151bc4e2235a38
Daniel Kim <DanielKim1@protonmail.com>
2022-04-29 10:14:22 -0400

    Enable sorting

--- candidates/views.py
+++ candidates/views.py
@@ -3,9 +3,10 @@ from json import dumps
 from csv import DictReader
 from os import path
 
-# FIXME https://docs.djangoproject.com/en/4.0/howto/static-files/
+# TODO https://docs.djangoproject.com/en/4.0/howto/static-files/
 # https://stackoverflow.com/questions/40066199/django-filenotfounderror-in-a-view-when-returning-html-using-codecs
-FILE = path.dirname(path.realpath(__file__)) + '/congressional-candidate-totals.csv'
+FILE = path.dirname(path.realpath(__file__)) + "/congressional-candidate-totals.csv"
+
 
 def index(request):
     result = []
@@ -13,15 +14,34 @@ def index(request):
         reader = DictReader(csvfile)
         for row in reader:
             result.append(row)
+
+    sort = request.GET.get("sort")
+    if sort:
+        key, direction = sort.split(":")
+        result.sort(
+            key=lambda candidate: candidate[key], reverse=direction.startswith("d")
+        )
+
     return HttpResponse(dumps(result))
+
 
 def election(request, state, year):
     result = []
     with open(FILE) as csvfile:
         reader = DictReader(csvfile)
         for row in reader:
-            if row['election_id'].startswith(state) and row['election_id'].endswith(year):
+            if row["election_id"].startswith(state) and row["election_id"].endswith(
+                year
+            ):
                 result.append(row)
+
+    sort = request.GET.get("sort")
+    if sort:
+        key, direction = sort.split(":")
+        result.sort(
+            key=lambda candidate: candidate[key], reverse=direction.startswith("d")
+        )
+
     if result:
         return HttpResponse(dumps(result))
-    return HttpResponseNotFound('no election found for {} in {}'.format(state, year))
+    return HttpResponseNotFound("no election found for {} in {}".format(state, year))
